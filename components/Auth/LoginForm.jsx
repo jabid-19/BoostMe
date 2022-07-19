c339ed7ec39d9e47783a759f4c52fde741f1f9ce
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-07-19 18:44:23 +0600

    Login button is changed to Sign In button

--- components/Auth/LoginForm.jsx
+++ components/Auth/LoginForm.jsx
@@ -24,7 +24,7 @@ const LoginForm = () => {
                         <input type="text" placeholder="Email address" className="block input input-bordered input-primary rounded-full w-full min-w-xs mb-4" />
                         <input type="text" placeholder="Password" className="block input input-bordered input-primary rounded-full w-full min-w-xs" />
                         <br />
-                        <input className='bg-secondary hover:bg-orange-400 py-1.5 w-full min-w-xs normal-case text-white rounded-full' type="submit" value="Login" />
+                        <input className='bg-secondary hover:bg-orange-400 py-1.5 w-full min-w-xs normal-case text-white rounded-full' type="submit" value="Sign In" />
                     </form>
                     <br />
                     <div className="flex justify-between w-full px-2">
