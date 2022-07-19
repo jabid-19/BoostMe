1fa98dabaea3295e428a695624cf383fbb584fcb
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-07-19 19:06:34 +0600

    ClassName from Image component in  ReactanglrLoginImage removed

--- components/RectangleLoginImage.jsx
+++ components/RectangleLoginImage.jsx
@@ -6,7 +6,7 @@ const RectangleLoginImage = () => {
     return (
         <div className='hidden lg:block lg:w-1/2'>
             <div className='lg:fixed lg:bottom-0 lg:left-0'>
-                <Image className='' width={520} height={520} src={RectangleImage} alt="" loader={imageLoader} />
+                <Image width={520} height={520} src={RectangleImage} alt="" loader={imageLoader} />
             </div>
         </div>
     );
