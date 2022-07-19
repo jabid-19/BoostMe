ce227473bee765e56434023be099a471e8430e01
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-07-19 18:56:17 +0600

    LoginFrameImage made visible for smaller device

--- components/LoginFrameImage.jsx
+++ components/LoginFrameImage.jsx
@@ -4,8 +4,8 @@ import imageLoader from '../helper/imageLoader';
 
 const LoginFrameImage = () => {
     return (
-        <div className='hidden lg:block lg:fixed lg:bottom-0 lg:right-0'>
-            <Image className='' width={500} height={117} src={FrameImage} alt="" loader={imageLoader} />
+        <div className='flex justify-center lg:fixed lg:bottom-0 lg:right-0'>
+            <Image width={500} height={117} src={FrameImage} alt="" loader={imageLoader} />
         </div>
     );
 }
