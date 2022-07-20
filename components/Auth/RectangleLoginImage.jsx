bc8954799664f37576c1df2d3738a26166fd86c1
jabid19 <jabid19191@gmail.com>
2022-07-20 13:15:48 +0600

    Image issue fixed

--- components/Auth/RectangleLoginImage.jsx
+++ components/Auth/RectangleLoginImage.jsx
@@ -1,14 +1,14 @@
-import Image from 'next/image';
-import RectangleImage from '../../public/login-section/Rectangle 10.png';
-import imageLoader from '../../helper/imageLoader';
+import Image from 'next/image'
+import RectangleImage from '../../public/login-section/Rectangle 10.png'
+import imageLoader from '../../helper/imageLoader'
 const RectangleLoginImage = () => {
-    return (
-        <div className='hidden lg:flex lg:items-end z-10'>
-            <div className='lg:h-3/4'>
-                <Image width={520} height={520} src={RectangleImage} alt="" loader={imageLoader} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="hidden lg:flex lg:items-end z-10">
+      <div className="lg:h-3/4">
+        <Image width={620} height={620} src={RectangleImage} alt="" loader={imageLoader} />
+      </div>
+    </div>
+  )
 }
 
-export default RectangleLoginImage;
+export default RectangleLoginImage
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,17 +1,19 @@
-import LoginForm from "../components/Auth/LoginForm";
-import LoginFrameImage from "../components/Auth/LoginFrameImage";
-import RectangleLoginImage from "../components/Auth/RectangleLoginImage";
+import LoginForm from '../components/Auth/LoginForm'
+import LoginFrameImage from '../components/Auth/LoginFrameImage'
+import RectangleLoginImage from '../components/Auth/RectangleLoginImage'
 
 const Login = () => {
-    return (
-        <div className="lg:grid lg:grid-cols-2">
-            <RectangleLoginImage />
-            <div>
-                <LoginForm />
-                <LoginFrameImage />
-            </div>
-        </div>
-    );
+  return (
+    <div className="lg:grid lg:grid-cols-2 min-h-screen">
+      <div className="flex items-end ">
+        <RectangleLoginImage />
+      </div>
+      <div>
+        <LoginForm />
+        <LoginFrameImage />
+      </div>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
