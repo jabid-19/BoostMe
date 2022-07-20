5ac6dceadf887012d1401f7d4638b023a8043da6
Tahmid <tahmid5550@gmail.com>
2022-07-20 13:14:05 +0600

    Testimonial section design done

--- components/home/section/Testimonials.jsx
+++ components/home/section/Testimonials.jsx
@@ -2,6 +2,13 @@ import Image from 'next/image'
 import React from 'react'
 
 const Testimonials = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
   return (
     <div className="min-h-screen mt-16">
       <div>
@@ -19,7 +26,95 @@ const Testimonials = () => {
         </div>
 
         {/* card design */}
-        <div></div>
+        <div className="flex flex-col flex-wrap gap-x-16 lg:flex-row lg:justify-around lg:items-center">
+          <div className="relative mx-auto md:max-w-sm mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-3xl border-solid border-4 border-primary">
+            <div className="px-6">
+              <div className="flex flex-wrap justify-center">
+                <div className="w-full flex justify-start -ml-[130px] -mt-[30px]">
+                  <div className="relative ">
+                    <Image
+                      src="/landing-section/Ellipse 3.png"
+                      className="border-solid border-4 border-primary shadow-xl rounded-full align-middle absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
+                      width={100}
+                      height="100%"
+                      alt="Landing"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-6 py-6">
+                <div className="flex flex-wrap justify-center">
+                  <div className="w-full px-4">
+                    <p className=" leading-relaxed text-neutral mb-4">
+                      On social, we need to be fast, efficient, and intentional. Boost allows us to
+                      be exactly that.
+                    </p>
+                    <p className="text-neutral font-bold">LUIS CANCEL</p>
+                    <p className="text-neutral font-bold">MANAGING EDITOR</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative mx-auto md:max-w-sm mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-3xl border-solid border-4 border-primary">
+            <div className="px-6">
+              <div className="flex flex-wrap justify-center">
+                <div className="w-full flex justify-start -ml-[130px] -mt-[30px]">
+                  <div className="relative ">
+                    <Image
+                      src="/landing-section/Ellipse 3.png"
+                      className="border-solid border-4 border-primary shadow-xl rounded-full align-middle absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
+                      width={100}
+                      height="100%"
+                      alt="Landing"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-6 py-6">
+                <div className="flex flex-wrap justify-center">
+                  <div className="w-full px-4">
+                    <p className=" leading-relaxed text-neutral mb-4">
+                      On social, we need to be fast, efficient, and intentional. Boost allows us to
+                      be exactly that.
+                    </p>
+                    <p className="text-neutral font-bold">LUIS CANCEL</p>
+                    <p className="text-neutral font-bold">MANAGING EDITOR</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative mx-auto md:max-w-sm mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-3xl border-solid border-4 border-primary">
+            <div className="px-6">
+              <div className="flex flex-wrap justify-center">
+                <div className="w-full flex justify-start -ml-[130px] -mt-[30px]">
+                  <div className="relative ">
+                    <Image
+                      src="/landing-section/Ellipse 3.png"
+                      className="border-solid border-4 border-primary shadow-xl rounded-full align-middle absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
+                      width={100}
+                      height="100%"
+                      alt="Landing"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-6 py-6">
+                <div className="flex flex-wrap justify-center">
+                  <div className="w-full px-4">
+                    <p className=" leading-relaxed text-neutral mb-4">
+                      On social, we need to be fast, efficient, and intentional. Boost allows us to
+                      be exactly that.
+                    </p>
+                    <p className="text-neutral font-bold">LUIS CANCEL</p>
+                    <p className="text-neutral font-bold">MANAGING EDITOR</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
