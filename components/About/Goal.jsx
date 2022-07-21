d6930a3690861b7c10cd30e536b7cfc3d02f0130
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-07-21 18:39:58 +0600

    Description component changed to Goal component

--- components/About/Goal.jsx
+++ components/About/Goal.jsx
@@ -2,7 +2,7 @@ const Goal = () => {
   return (
     <div className="bg-[url('https://images.unsplash.com/photo-1634876679764-422a0414d72b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80')] bg-no-repeat bg-cover mt-36">
       <div className="py-12 px-8 lg:max-w-5xl lg:mx-auto">
-        <h1 className="text-3xl lg:text-5xl pb-6">Description</h1>
+        <h1 className="text-3xl lg:text-5xl pb-6">Goal</h1>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia tenetur deleniti sint
           maiores animi facilis suscipit minima eveniet quo assumenda!
