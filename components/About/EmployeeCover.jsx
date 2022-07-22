048a55ee4013634e20bbaf3c48a7948b45bd072b
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-07-22 18:23:12 +0600

    removed some comments

--- components/About/EmployeeCover.jsx
+++ components/About/EmployeeCover.jsx
@@ -1,9 +1,8 @@
 const EmployeeCover = () => {
   return (
-    // <div className="min-h-screen bg-[url('https://images.unsplash.com/photo-1634876679764-422a0414d72b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80')] bg-no-repeat bg-cover mt-36 flex justify-center items-center">
     <div
       style={{ backgroundImage: `url('/landing-section/Group 28.png')` }}
-      className="min-h-screen bg-[url('https://images.unsplash.com/photo-1634876679764-422a0414d72b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80')] bg-no-repeat bg-cover mt-36 flex justify-center items-center">
+      className="min-h-screen bg-no-repeat bg-cover mt-36 flex justify-center items-center">
       <div className="text-center text-white">
         <h1 className="text-5xl lg:text-7xl">We who worked on this site</h1>
         <h2 className="text-3xl py-6">Where the passion begins</h2>
