d50f0b78347290a669e2c746868e9c780db65abd
Tahmid <tahmid5550@gmail.com>
2022-07-22 18:17:28 +0600

    Tabs added

--- components/StartPage/section/Showcase.jsx
+++ components/StartPage/section/Showcase.jsx
@@ -1,27 +1,96 @@
+import Image from 'next/image'
 import React, { useState } from 'react'
 
 const Showcase = () => {
   const [tabs, setTabs] = useState(1)
 
   return (
-    <div className="flex items-center flex-col justify-center w-auto mx-6 lg:h-screen 2xl:w-[1400px] 2xl:mx-auto mb-20">
-      <div className="tabs">
+    <div className="flex items-center flex-col w-auto mx-6 lg:h-screen 2xl:w-[1400px] 2xl:mx-auto mb-20">
+      <div className="tabs mb-10">
         <a
           className={`tab tab-lg tab-lifted ${tabs === 1 && 'tab-active'}`}
           onClick={() => setTabs(1)}>
-          Tab 1
+          Small Business
         </a>
         <a
           className={`tab tab-lg tab-lifted ${tabs === 2 && 'tab-active'}`}
           onClick={() => setTabs(2)}>
-          Tab 2
+          Social Media
         </a>
         <a
           className={`tab tab-lg tab-lifted ${tabs === 3 && 'tab-active'}`}
           onClick={() => setTabs(3)}>
-          Tab 3
+          NFTs
         </a>
       </div>
+      {tabs === 1 && (
+        <div className="flex flex-col-reverse lg:flex-row gap-10 w-full md:flex-row">
+          <div className="w-[90%] mx-auto flex flex-col justify-center item-center lg:w-1/2 ">
+            <h1 className="text-4xl text-center font-semibold break-words text-neutral md:text-start lg:tracking-wide xl:text-5xl">
+              Build a loyal and <span className="text-primary">audience</span> on
+              <br />
+              <span className="text-secondary">social media</span>
+            </h1>
+            <p className="text-neutral text-center text-base mt-10 md:text-start">
+              See all of your comments, stay on top of important <br /> interactions, and turn
+              followers into fans <br /> using Buffer.
+            </p>
+            <button className="btn btn-wide rounded-full mt-10 btn-primary mx-auto text-white md:mx-0">
+              Create Your Page For Free
+            </button>
+          </div>
+          <div className="w-[95%] mx-auto flex lg:w-1/2 ">
+            <Image src="/start-page/customize-1.png" width={650} height={600} alt="Engagement" />
+          </div>
+        </div>
+      )}
+      {tabs === 2 && (
+        <div className="flex flex-col gap-10 w-full md:flex-row">
+          <div className="w-[95%] mx-auto flex justify-center lg:w-1/2 ">
+            <Image
+              src="/start-page/Small-business-1.png"
+              width={550}
+              height={620}
+              alt="Engagement"
+            />
+          </div>
+          <div className="w-[90%] mx-auto flex flex-col justify-center item-center lg:w-1/2 ">
+            <h1 className="text-4xl text-center font-semibold break-words text-neutral md:text-start lg:tracking-wide xl:text-5xl">
+              Build a loyal and <span className="text-primary">audience</span> on
+              <br />
+              <span className="text-secondary">social media</span>
+            </h1>
+            <p className="text-neutral text-center text-base mt-10 md:text-start">
+              See all of your comments, stay on top of important <br /> interactions, and turn
+              followers into fans <br /> using Buffer.
+            </p>
+            <button className="btn btn-wide rounded-full mt-10 btn-primary mx-auto text-white md:mx-0">
+              Create Your Page For Free
+            </button>
+          </div>
+        </div>
+      )}
+      {tabs === 3 && (
+        <div className="flex flex-col-reverse gap-10 w-full md:flex-row">
+          <div className="w-[90%] mx-auto flex flex-col justify-center item-center lg:w-1/2 ">
+            <h1 className="text-4xl text-center font-semibold break-words text-neutral md:text-start lg:tracking-wide xl:text-5xl">
+              Build a loyal and <span className="text-primary">audience</span> on
+              <br />
+              <span className="text-secondary">social media</span>
+            </h1>
+            <p className="text-neutral text-center text-base mt-10 md:text-start">
+              See all of your comments, stay on top of important <br /> interactions, and turn
+              followers into fans <br /> using Buffer.
+            </p>
+            <button className="btn btn-wide rounded-full mt-10 btn-primary mx-auto text-white md:mx-0">
+              Create Your Page For Free
+            </button>
+          </div>
+          <div className="w-[95%] mx-auto flex justify-center lg:w-1/2 ">
+            <Image src="/start-page/NFT-1.png" width={550} height={620} alt="Engagement" />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
