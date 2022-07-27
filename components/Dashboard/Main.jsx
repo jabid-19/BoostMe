d7d1770604fc7587a17d98914db38c4446f4d2db
Tahmid <tahmid5550@gmail.com>
2022-07-27 17:09:33 +0600

    Files refactored

--- components/Dashboard/Main.jsx
+++ components/Dashboard/Main.jsx
@@ -1,74 +1,17 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
-import { AiOutlineBarChart, AiOutlineFileSync, AiOutlineTeam } from 'react-icons/ai'
-import { BiCalendar, BiSearchAlt } from 'react-icons/bi'
-import { BsChat, BsKanban } from 'react-icons/bs'
-import { FiSettings } from 'react-icons/fi'
-import { RiAccountPinBoxLine } from 'react-icons/ri'
+import { Menus, tabsMenu } from '../../data/Dashboard'
 const DashboardMain = () => {
-  const tabsMenu = [
-    {
-      id: 1,
-      title: 'Publishing',
-      icons: false,
-    },
-    {
-      id: 2,
-      title: 'Analytics',
-      icons: false,
-    },
-    {
-      id: 3,
-      title: 'Engagements',
-      icons: false,
-    },
-    {
-      id: 4,
-      title: 'Start Page',
-      icons: false,
-    },
-    {
-      id: 5,
-      title: 'Invite Your Team',
-      icons: true,
-      iconName: <AiOutlineTeam />,
-    },
-    {
-      id: 6,
-      title: 'Apps',
-      icons: false,
-    },
-    {
-      id: 7,
-      title: 'Help',
-      icons: false,
-    },
-    {
-      id: 8,
-      title: 'Email',
-      icons: false,
-    },
-  ]
-  const Menus = [
-    { title: 'Dashboard', icon: <AiOutlineBarChart size={20} /> },
-    { title: 'Inbox', icon: <BsChat size={20} /> },
-    { title: 'Accounts', gap: true, icon: <RiAccountPinBoxLine size={20} /> },
-    { title: 'Schedule', icon: <BiCalendar size={20} /> },
-    { title: 'Search', icon: <BiSearchAlt size={20} /> },
-    { title: 'Analytics', icon: <BsKanban size={20} /> },
-    { title: 'Files', gap: true, icon: <AiOutlineFileSync size={20} /> },
-    { title: 'Settings', icon: <FiSettings size={20} /> },
-  ]
   const [activeTab, setActiveTab] = useState(null)
   const [open, setOpen] = useState(true)
   return (
     <div className="min-h-screen">
-      <div className="tabs flex justify-around p-4">
+      <div className="tabs flex justify-around p-4 border-b-2">
         {tabsMenu.map((menu) => (
           <a
             className={`tab font-bold text-neutral text-[16px] ${
               activeTab === menu.id && 'text-primary'
-            }`}
+            } hover:text-primary`}
             onClick={() => setActiveTab(menu.id)}
             key={menu.id}>
             {menu.icons && menu.iconName}
@@ -76,8 +19,8 @@ const DashboardMain = () => {
           </a>
         ))}
       </div>
-      <div className="flex">
-        <div className={`${open ? 'w-72' : 'w-20'} duration-300 w-72 h-screen bg-primary relative`}>
+      <div className="flex fixed">
+        <div className={`${open ? 'w-72' : 'w-20'} duration-300 w-72 h-[94vh] bg-primary relative`}>
           <div
             className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-primary ${
               !open && 'rotate-180'
@@ -101,7 +44,7 @@ const DashboardMain = () => {
                 }`}
                 key={index}>
                 {menu.icon}
-                <span className={`${!open && 'hidden'} origin-left duration-200`}>
+                <span className={`${!open && 'hidden'} origin-left duration-200 text-xl`}>
                   {menu.title}
                 </span>
               </li>
