367004c1eef53d06620f0954101e48a32e599558
jabid19 <jabid19191@gmail.com>
2022-07-29 22:47:34 +0600

    Changed image path

--- helper/imageLoader.js
+++ helper/imageLoader.js
@@ -1,4 +1,5 @@
 const imageLoader = ({ src, width, quality }) => {
-  return `http://localhost:3000/${src}?w=${width}&q=${quality || 80}}`
+  // return `http://localhost:3000/${src}?w=${width}&q=${quality || 80}}`
+  return `http://3.249.53.40/${src}?w=${width}&q=${quality || 80}}`
 }
 export default imageLoader
