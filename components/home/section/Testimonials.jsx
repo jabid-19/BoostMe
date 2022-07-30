b1d6873dc22870564d19880b3e3a9291c3c96aba
Tahmid <tahmid5550@gmail.com>
2022-07-30 16:18:50 +0600

    slider auto play and removed unused packages

--- components/home/section/Testimonials.jsx
+++ components/home/section/Testimonials.jsx
@@ -2,9 +2,6 @@ import Image from 'next/image'
 import React from 'react'
 import Slider from 'react-slick'
 
-import 'swiper/css'
-import 'swiper/css/pagination'
-
 // Import css files
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
@@ -16,9 +13,9 @@ const Testimonials = () => {
   var settings = {
     dots: true,
     infinite: true,
-    speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    autoplay: true,
     responsive: [
       {
         breakpoint: 1024,
