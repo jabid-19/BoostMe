970e309256d0caaabbf3f89a4d2d20aa06477c5e
Tahmid <tahmid5550@gmail.com>
2022-08-11 16:48:58 +0600

    Process bar created

--- components/Dashboard/Publishing/Create-Post/Sections/Forms.jsx
+++ components/Dashboard/Publishing/Create-Post/Sections/Forms.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import FormCard from './Forms/FormCard'
 
 const Forms = () => {
-  const [formStep, setFormStep] = useState(0)
+  const [formStep, setFormStep] = useState(1)
 
   const nextFormStep = () => setFormStep((currentStep) => currentStep + 1)
 
   const prevFormStep = () => setFormStep((currentStep) => currentStep - 1)
+
   return <FormCard currentStep={formStep} prevFormStep={prevFormStep} nextFormStep={nextFormStep} />
 }
 
--- components/Dashboard/Publishing/Create-Post/Sections/Forms/FormCard.jsx
+++ components/Dashboard/Publishing/Create-Post/Sections/Forms/FormCard.jsx
@@ -1,10 +1,64 @@
 export default function FormCard({ children, currentStep, prevFormStep, nextFormStep }) {
+  console.log(currentStep)
   return (
-    <ul className="steps gap-5">
-      <li className="step step-primary">Step 1</li>
-      <li className="step step-primary">Step 2</li>
-      <li className="step">Step 3</li>
-      <li className="step">Step 4</li>
-    </ul>
+    <div className="flex flex-col">
+      <ul className="steps gap-5">
+        <li className={`${currentStep === 1 || currentStep >= 1 ? 'step step-primary' : 'step'}`}>
+          Step 1
+        </li>
+        <li className={`${currentStep === 2 || currentStep >= 2 ? 'step step-primary' : 'step'}`}>
+          Step 2
+        </li>
+        <li className={`${currentStep === 3 || currentStep >= 3 ? 'step step-primary' : 'step'}`}>
+          Step 3
+        </li>
+        <li className={`${currentStep === 4 ? 'step step-primary' : 'step'}`}>Step 4</li>
+      </ul>
+      <div className="flex justify-between my-10">
+        <button
+          className="
+                inline-flex
+                items-center
+                px-6
+                py-2
+                text-sm text-gray-800
+                rounded-lg
+                shadow
+                outline-none
+                gap-x-1
+                hover:bg-gray-100
+            "
+          onClick={currentStep <= 4 && currentStep > 1 ? prevFormStep : null}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-4 h-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+          Back
+        </button>
+        <button
+          className="
+                px-6
+                py-2
+                text-sm text-white
+                bg-indigo-500
+                rounded-lg
+                outline-none
+                hover:bg-indigo-600
+                ring-indigo-300
+            "
+          onClick={currentStep < 4 && currentStep >= 1 ? nextFormStep : null}>
+          Next
+        </button>
+      </div>
+    </div>
   )
 }
