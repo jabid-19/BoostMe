2ef21c66747e4c21688d2c411b7c268f582e5a41
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2022-08-13 17:33:51 +0600

    axios baseURL changed

--- src/backend/backendApi.js
+++ src/backend/backendApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const backendApi = axios.create({
   // baseURL: 'http://54.78.229.191:8800/',
   // baseURL: 'http://localhost:8800',
-  baseURL: 'https://boostnboom.com/api',
+  baseURL: 'https://api.boostnboom.com',
 })
 
 export default backendApi
