a15311716852529349843ffbf75b7360409ebc42
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-08-26 17:06:19 +0600

    navbar components changed for sign in and sign out and items style changed in about page and FAQ changed in pricing page

--- components/Pricing/FaqSection.js
+++ components/Pricing/FaqSection.js
@@ -2,23 +2,49 @@ import React from 'react'
 
 const FaqSection = ({ faqQuestions }) => {
   return (
-    <div data-aos="flip-up" className="container mx-auto my-24">
-      <div className="sectionHeader mb-5">
-        <p className="text-2xl font-bold text-center my-10">FAQ</p>
-      </div>
-      {faqQuestions.map((item, index) => {
-        return (
-          <div
-            key={index}
-            tabIndex="0"
-            className="collapse collapse-arrow border border-primary-300 bg-base-100 rounded-box lg:w-2/3 mx-auto mt-2 hover:border-secondary duration-300">
-            <div className="collapse-title text-xl font-medium">{item.question}</div>
-            <div className="collapse-content">
-              <p> {item.answer} </p>
-            </div>
+    // <div data-aos="flip-up" className="container mx-auto my-24">
+    //   <div className="sectionHeader mb-5">
+    //     <p className="text-2xl font-bold text-center my-10">FAQ</p>
+    //   </div>
+    //   {faqQuestions.map((item, index) => {
+    //     return (
+    //       <div
+    //         key={index}
+    //         tabIndex="0"
+    //         className="collapse collapse-arrow border border-primary-300 bg-base-100 rounded-box lg:w-2/3 mx-auto mt-2 hover:border-secondary duration-300">
+    //         <div className="collapse-title text-xl font-medium">{item.question}</div>
+    //         <div className="collapse-content">
+    //           <p> {item.answer} </p>
+    //         </div>
+    //       </div>
+    //     )
+    //   })}
+    //   </div>
+    <div
+      data-aos="zoom-in"
+      className="flex items-center justify-center w-auto mx-6 my-24 2xl:w-[1400px] 2xl:mx-auto">
+      <section className="w-full">
+        <div className="flex flex-col justify-center px-4 py-8  md:p-8">
+          <div>
+            <h1 className="text-4xl text-center font-semibold break-words text-neutral lg:tracking-wide xl:text-5xl">
+              Frequently Asked Questions
+            </h1>
+            <p className="mt-4 mb-8 text-neutral text-center">Questions asked by many users</p>
           </div>
-        )
-      })}
+          <div className="space-y-4">
+            {faqQuestions.map((item, index) => (
+              <div key={index}>
+                <details className="border border-primary rounded-lg">
+                  <summary className="px-4 py-6 font-bold text-neutral">{item.question}</summary>
+                  <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-neutral text-justify">
+                    {item.answer}
+                  </p>
+                </details>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
--- components/Shared/Navbar.js
+++ components/Shared/Navbar.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import imageLoader from '../../helper/imageLoader'
 import Logo from '../../public/common/logo-navbar.png'
+import { CgProfile } from 'react-icons/cg'
 
 // import axios from '../../axios'
 
@@ -123,16 +124,61 @@ const Navbar = () => {
                 BN
               </button>
             </div> */}
-            <li>
-              <Link href="login">
-                <a>{email || 'Login'}</a>
-              </Link>
-            </li>
-            <li>
-              <Link href={{ pathname: 'login', query: { keyword: 'register' } }}>
-                <a className="btn btn-primary text-white mt-1.5">Get Started</a>
-              </Link>
-            </li>
+            {email && (
+              <li tabIndex="0">
+                <a className="justify-between">
+                  <div className="flex flex-col space-y-2">
+                    <p>{email}</p>
+                    {/* <div className="avatar placeholder">
+                    <div className="bg-primary-focus text-neutral-content rounded-full w-12 h-12">
+                      <span>
+                        <CgProfile className="w-12 h-12" />
+                      </span>
+                    </div>
+                  </div> */}
+                  </div>
+                  <svg
+                    className="fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24">
+                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                  </svg>
+                </a>
+                <ul className="p-2 bg-white">
+                  <li>
+                    <Link href="/dashboard">
+                      <a className="pl-4">Dashboard</a>
+                    </Link>
+                  </li>
+                  {/* <li>
+                <Link href="/profile">
+                  <a className="pl-4">Profile</a>
+                </Link>
+              </li> */}
+                  <li>
+                    <Link href="/login">
+                      <a className="pl-4">Sign out</a>
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            )}
+            {!email && (
+              <li>
+                <Link href="login">
+                  <a>Login</a>
+                </Link>
+              </li>
+            )}
+            {!email && (
+              <li>
+                <Link href={{ pathname: 'login', query: { keyword: 'register' } }}>
+                  <a className="btn btn-primary text-white mt-1.5">Get Started</a>
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
         <Link href="/">
@@ -223,51 +269,59 @@ const Navbar = () => {
               BN
             </button>
           </div> */}
-          {/* <li tabIndex="0">
-            <a>
-              <p>Jhon Doe</p>
-              <div className="avatar placeholder">
-                <div className="bg-primary-focus text-neutral-content rounded-full w-12 h-12">
-                  <span>JD</span>
+          {email && (
+            <li tabIndex="0">
+              <a>
+                <p>{email}</p>
+                <div className="avatar placeholder">
+                  <div className="bg-primary-focus text-neutral-content rounded-full w-12 h-12">
+                    <span>
+                      <CgProfile className="w-12 h-12" />
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24">
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2 bg-white">
-              <li>
-                <a className="px-10" href="publishing">
-                  Publishing
-                </a>
-              </li>
-              <li>
-                <Link href="/engagement">
-                  <a className="px-10">Engagement</a>
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24">
+                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                </svg>
+              </a>
+              <ul className="p-2 bg-white w-full">
+                <li>
+                  <Link href="/dashboard">
+                    <a className="pl-4">Dashboard</a>
+                  </Link>
+                </li>
+                {/* <li>
+                <Link href="/profile">
+                  <a className="pl-4">Profile</a>
                 </Link>
-              </li>
-              <li>
-                <Link href="/start-page">
-                  <a className="px-10">Start Page</a>
-                </Link>
-              </li>
-            </ul>
-          </li> */}
-          <li className="text-primary mr-4">
-            <Link href="login">
-              <a>{email || 'Login'}</a>
+              </li> */}
+                <li>
+                  <Link href="/login">
+                    <a className="pl-4">Sign out</a>
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          )}
+          {!email && (
+            <li className="text-primary mr-4">
+              <Link href="login">
+                <a>Login</a>
+              </Link>
+            </li>
+          )}
+          {!email && (
+            <Link href={{ pathname: 'login', query: { keyword: 'register' } }}>
+              <a className="btn btn-primary text-white text-center capitalize rounded-full px-8">
+                Get Started
+              </a>
             </Link>
-          </li>
-          <Link href={{ pathname: 'login', query: { keyword: 'register' } }}>
-            <a className="btn btn-primary text-white text-center capitalize rounded-full px-8">
-              Get Started
-            </a>
-          </Link>
+          )}
           {/* <Link href="/contact">
             <a className="btn btn-primary text-white text-center normal-case rounded-full px-8">
               Contact us
