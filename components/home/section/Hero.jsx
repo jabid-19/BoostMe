1971ee1d2baa6bacfbc62de29f570aacb9c5b612
Tahmid <tahmid5550@gmail.com>
2022-08-30 18:10:36 +0600

    ui fixed for cross browser

--- components/home/section/Hero.jsx
+++ components/home/section/Hero.jsx
@@ -1,108 +1,90 @@
 import Image from 'next/image'
-import React from 'react'
 import Link from 'next/link'
-// import { BsPlayBtn } from 'react-icons/bs'
+import React from 'react'
 import imageLoader from '../../../helper/imageLoader'
 import Background from '../../../public/common/BoxBackground.png'
 const Hero = () => {
   return (
-    <div className="min-h-[85vh] w-full bg-cover bg-center hero place-items-center mt-12 mb-12 lg:mb-0">
-      <div className="flex flex-col justify-center gap-4 h-3/5 items-center lg:flex-row">
-        <div data-aos="fade-right" className="flex justify-around items-center gap-4 lg:flex-col">
+    <div className="w-full relative">
+      <div className="flex flex-col w-auto mx-6 mb-20 mt-10 lg:flex-row lg:h-[85vh] lg:mb-0 2xl:w-[1200px] 2xl:mx-auto gap-4">
+        <div className="w-full flex flex-row gap-4 items-center justify-center lg:flex-col lg:w-1/4 lg:items-end">
           <Image
             src="/landing-section/SocialMedia_Facebook.png"
             width={240}
-            height={270}
+            height={370}
             alt="Facebook Digital Marketing"
-            className="max-w-full h-auto rounded-lg"
+            className="object-cover rounded-lg"
             loader={imageLoader}
             priority={true}
           />
           <Image
             src="/landing-section/SocialMedia_Instagram.png"
             width={240}
-            height={270}
+            height={370}
             alt="Instagram Digital Marketing"
-            className="max-w-full h-auto rounded-lg"
+            className="object-cover rounded-lg"
             loader={imageLoader}
             priority={true}
           />
         </div>
-        <div className="flex flex-col items-center justify-start lg:items-center lg:justify-start">
-          <div
-            data-aos="fade-left"
-            className="h-auto text-center lg:text-left mb-10 max-w-fit z-20">
-            <h1 className="text-2xl font-semibold break-words md:text-4xl text-neutral lg:tracking-wide xl:text-5xl">
+        <div className="w-full flex flex-col justify-center items-center lg:w-3/4">
+          <div className="h-auto mx-auto my-5 lg:ml-28 lg:my-0">
+            <h1 className="text-2xl mb-5 font-semibold break-words md:text-4xl text-neutral lg:tracking-wide xl:text-5xl">
               <span className="text-primary">বুস্টিং</span> করুন{' '}
               <span className="text-secondary">সহজে</span>
-              {/* Build your <span className="text-primary">audience</span> and
-              <br />
-              <span className="text-secondary">grow</span> your brand */}
             </h1>
           </div>
-          <div className="h-auto flex flex-col items-center justify-center lg:flex-row lg:items-start z-20">
-            <div data-aos="fade-up">
+          <div className="w-full h-auto flex items-center flex-col gap-4 lg:flex-row">
+            <div className="flex justify-center">
               <Image
                 src="/landing-section/SocialMedia_Likes.png"
-                width={240}
-                height={270}
-                alt="Social Media Likes"
-                className="max-w-full h-auto rounded-lg"
+                width={310}
+                height={400}
+                alt="Facebook Digital Marketing"
+                className="max-w-full object-cover rounded-lg"
                 loader={imageLoader}
                 priority={true}
               />
             </div>
-            <div data-aos="fade-left">
-              <div className="flex flex-col items-center justify-center lg:justify-start lg:flex-row lg:ml-8 lg:items-start text-center lg:text-left">
-                <p className="w-full lg:w-3/4 max-w-fit text-neutral">
-                  বুস্ট করুন আপনার পছন্দের সোশ্যাল মিডিয়া প্ল্যাটফর্মে কোনরকম ক্রেডিট কার্ড ছাড়াই।
-                  বিকাশ, নগদ, ডেবিট কার্ড কিংবা ব্যাংক ট্রান্সফারের মাধ্যমে আপনার সকল সোশ্যাল
-                  মিডিয়া প্ল্যাটফর্ম যেকোনো পোস্ট প্রমোট করতে পারবেন BoostnBoom অ্যাপ থেকে।
-                  {/* Plan and publish your content for Instagram, <br /> TikTok, Facebook, Twitter,
-                  Pinterest, and LinkedIn,
-                  <br /> all from one simple dashboard. */}
-                </p>
-              </div>
-              <div className="flex flex-col mt-4 items-center justify-center lg:justify-start md:flex-row lg:ml-8">
-                <div className="flex flex-col items-center md:flex-row">
-                  <div className="mb-4 md:mr-4 md:mb-0">
-                    <Link href="login">
-                      <a>
-                        <button className="btn btn-primary normal-case px-12 text-white rounded-full">
-                          Get Started
-                        </button>
-                      </a>
-                    </Link>
-                  </div>
-                  <div>
-                    {/* <button
-                      className="btn btn-primary normal-case px-10 text-primary rounded-full bg-white hover:bg-transparent">
-                      <BsPlayBtn />
-                      <span className="ml-2">Watch Video</span>
-                    </button> */}
-                    <Link href="contact">
-                      <a>
-                        <button className="btn btn-primary normal-case px-12 text-primary rounded-full bg-white hover:bg-transparent">
-                          Contact Us
-                        </button>
-                      </a>
-                    </Link>
-                  </div>
+            <div className="w-full">
+              <p className="max-w-fit text-neutral lg:w-1/2">
+                বুস্ট করুন আপনার পছন্দের সোশ্যাল মিডিয়া প্ল্যাটফর্মে কোনরকম ক্রেডিট কার্ড ছাড়াই।
+                বিকাশ, নগদ, ডেবিট কার্ড কিংবা ব্যাংক ট্রান্সফারের মাধ্যমে আপনার সকল সোশ্যাল মিডিয়া
+                প্ল্যাটফর্ম যেকোনো পোস্ট প্রমোট করতে পারবেন BoostnBoom অ্যাপ থেকে।
+              </p>
+              <div className="mt-4 w-full flex flex-col items-center gap-4  lg:w-3/4 lg:flex-row lg:ml-4 xl:ml-0">
+                <div className="">
+                  <Link href="login">
+                    <a>
+                      <button className="btn btn-primary normal-case px-12 text-white rounded-full">
+                        Get Started
+                      </button>
+                    </a>
+                  </Link>
+                </div>
+                <div className="">
+                  <Link href="contact">
+                    <a>
+                      <button className="btn btn-primary normal-case px-12 text-primary rounded-full bg-white hover:bg-transparent">
+                        Contact Us
+                      </button>
+                    </a>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
-          <div className="hidden lg:block lg:absolute lg:bottom-0 lg:right-0 lg:h-[100px] lg:w-[500px] ">
-            <Image
-              src={Background}
-              width={500}
-              height={100}
-              alt="BoostnBoom's Background"
-              className="max-w-full h-auto rounded-lg"
-              loader={imageLoader}
-            />
-          </div>
         </div>
+      </div>
+      <div className="hidden lg:block lg:absolute lg:bottom-0 lg:right-0 lg:h-[100px] lg:w-[500px] ">
+        <Image
+          src={Background}
+          width={500}
+          height={100}
+          alt="BoostnBoom's Background"
+          className="max-w-full h-auto rounded-lg"
+          loader={imageLoader}
+        />
       </div>
     </div>
   )
