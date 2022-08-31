9d46151265f828271072ea04ec5e8dd93148273b
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-08-31 17:34:23 +0600

    selection for previous time handled

--- components/Dashboard/Publishing/Calendar/Calendar.jsx
+++ components/Dashboard/Publishing/Calendar/Calendar.jsx
@@ -1,30 +1,27 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Calendar as BigCalendar, momentLocalizer, Views } from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 // import 'react-big-calendar/lib/sass/styles'
 const localizer = momentLocalizer(moment)
-console.log('localizer', localizer)
-// const myEventsList = [
-//   { id: 0, start: new Date(), end: new Date(), title: 'special event' },
-//   { id: 1, start: new Date(), end: new Date(), title: 'special event2' },
-//   { id: 2, start: new Date(), end: new Date(), title: 'special event3' },
-// ]
+// console.log('localizer', localizer)
+// import PropTypes from 'prop-types'
+// import { Navigate } from 'react-big-calendar'
+// import TimeGrid from 'react-big-calendar/lib/TimeGrid'
 
+// some dummy events to check out if the schedules events are shown in the calendar
 // const events = [
 //   {
 //     id: 0,
 //     title: 'Board meeting',
 //     start: new Date(2022, 7, 17, 9, 0, 0),
 //     end: new Date(2022, 7, 17, 11, 0, 0),
-//     // resourceId: 1,
 //   },
 //   {
 //     id: 1,
 //     title: 'MS training',
 //     start: new Date(2022, 7, 18, 14, 30, 0),
 //     end: new Date(2022, 7, 18, 16, 30, 0),
-//     // resourceId: 2,
 //   },
 //   {
 //     id: 2,
@@ -32,44 +29,101 @@ console.log('localizer', localizer)
 //     // allDay: true,
 //     start: new Date(2022, 7, 16, 8, 30, 0),
 //     end: new Date(2022, 7, 19, 11, 30, 0),
-//     // resourceId: 3,
 //   },
 //   {
 //     id: 3,
 //     title: 'Birthday Party',
 //     start: new Date(2022, 7, 19, 7, 0, 0),
 //     end: new Date(2022, 7, 19, 10, 30, 0),
-//     // resourceId: 4,
 //   },
 // ]
 
-// const resourceMap = [
-//   { resourceId: 1, resourceTitle: 'Board room' },
-//   { resourceId: 2, resourceTitle: 'Training room' },
-//   { resourceId: 3, resourceTitle: 'Meeting room 1' },
-//   { resourceId: 4, resourceTitle: 'Meeting room 2' },
-// ]
+// this function is for custom week view
+// function MyWeek({
+//   date,
+//   localizer,
+//   max = localizer.endOf(new Date(), 'day'),
+//   min = localizer.startOf(new Date(), 'day'),
+//   scrollToTime = localizer.startOf(new Date(), 'day'),
+//   ...props
+// }) {
+//   const currRange = useMemo(() => MyWeek.range(date, { localizer }), [date, localizer])
 
-// const styles = {
-//   container: {
-//     width: '80wh',
-//     height: '75vh',
-//     margin: '2em',
-//   },
+//   return (
+//     <TimeGrid
+//       date={date}
+//       eventOffset={15}
+//       localizer={localizer}
+//       max={max}
+//       min={min}
+//       range={currRange}
+//       scrollToTime={scrollToTime}
+//       {...props}
+//     />
+//   )
+// }
+
+// MyWeek.propTypes = {
+//   date: PropTypes.instanceOf(Date).isRequired,
+//   localizer: PropTypes.object,
+//   max: PropTypes.instanceOf(Date),
+//   min: PropTypes.instanceOf(Date),
+//   scrollToTime: PropTypes.instanceOf(Date),
+// }
+
+// MyWeek.range = (date, { localizer }) => {
+//   const start = date
+//   const end = localizer.add(start, 7, 'day')
+
+//   let current = start
+//   const range = []
+
+//   while (localizer.lte(current, end, 'day')) {
+//     range.push(current)
+//     current = localizer.add(current, 1, 'day')
+//   }
+
+//   return range
+// }
+
+// MyWeek.navigate = (date, action, { localizer }) => {
+//   switch (action) {
+//     case Navigate.PREVIOUS:
+//       return localizer.add(date, -8, 'day')
+
+//     case Navigate.NEXT:
+//       return localizer.add(date, 8, 'day')
+
+//     default:
+//       return date
+//   }
+// }
+
+// MyWeek.title = (date) => {
+//   return <div className="text-3xl">Week: {date.toLocaleDateString()}</div>
 // }
 
 const Calendar = () => {
+  const [view, setView] = useState(Views.MONTH)
+  const onView = useCallback((newView) => setView(newView), [setView])
+
   const [isOpen, setIsOpen] = useState(false)
 
   const CURRENT_DATE = moment().toDate()
-  console.log('current date', CURRENT_DATE)
+  // console.log('current date', CURRENT_DATE)
 
   const onSelectSlot = (event) => {
-    // window.alert('Slot is selected')
-    console.log('Event', event)
-    if (moment(event.start).isBefore(CURRENT_DATE, 'day')) {
-      setIsOpen(false)
-    } else setIsOpen(true)
+    // console.log('Event', event)
+    if (view === 'month') {
+      if (moment(event.start).isBefore(CURRENT_DATE, 'day')) {
+        setIsOpen(false)
+      } else setIsOpen(true)
+    }
+    if (view === 'week') {
+      if (moment(event.start).isBefore(CURRENT_DATE, 'minute')) {
+        setIsOpen(false)
+      } else setIsOpen(true)
+    }
   }
 
   const isOpenState = () => {
@@ -84,8 +138,7 @@ const Calendar = () => {
   //       backgroundColor: value < CURRENT_DATE ? 'lightgreen' : 'lightblue',
   //     },
   //   })
-  // const today = new Date()
-  // const day = today.getDay()
+
   const dayPropGetter = (date) => {
     let backgroundColor
     let opacity
@@ -94,7 +147,6 @@ const Calendar = () => {
 
     if (moment(date).isBefore(CURRENT_DATE, 'day')) {
       backgroundColor = '#e8e8e8'
-      // opacity = 0.5
       cursor = 'default'
     }
     if (moment(date).isSameOrAfter(CURRENT_DATE, 'day')) {
@@ -105,14 +157,7 @@ const Calendar = () => {
       opacity = 0.5
       color = 'white'
     }
-    // if (moment(date).isBefore(CURRENT_DATE, 'month')) {
-    //   backgroundColor = '#FCAF45'
-    //   opacity = 0.25
-    // }
-    // if (moment(date).isAfter(CURRENT_DATE, 'month')) {
-    //   backgroundColor = '#FCAF45'
-    //   opacity = 0.5
-    // }
+
     var style = {
       backgroundColor,
       opacity,
@@ -122,20 +167,14 @@ const Calendar = () => {
     return {
       style: style,
     }
-    // ...(moment(date).day() < day && {
-    //   style: {
-    //     backgroundColor: 'red',
-    //     color: 'white',
-    //   },
-    // }),
   }
 
+  // it's for taking the slots property
   // const slotPropGetter = (date) => {
-  //   const CURRENT_DATE = moment().toDate()
   //   let backgroundColor
 
-  //   if (moment(date).isBefore(CURRENT_DATE, 'month')) {
-  //     backgroundColor = '#f7f8f9'
+  //   if (moment(date).isBefore(CURRENT_DATE, 'day')) {
+  //     backgroundColor = 'red'
   //   }
 
   //   var style = {
@@ -146,6 +185,17 @@ const Calendar = () => {
   //   }
   // }
 
+  // const { views } = useMemo(
+  //   () => ({
+  //     views: {
+  //       month: true,
+  //       week: MyWeek,
+  //       agenda: true,
+  //     },
+  //   }),
+  //   []
+  // )
+
   return (
     <div>
       <div className="-z-50">
@@ -155,6 +205,7 @@ const Calendar = () => {
           localizer={localizer}
           // events={events}
           defaultView={Views.MONTH}
+          // views={views}
           views={[Views.WEEK, Views.MONTH, Views.AGENDA]}
           startAccessor="start"
           endAccessor="end"
@@ -165,7 +216,7 @@ const Calendar = () => {
           dayPropGetter={dayPropGetter}
           // components={{
           //   dateCellWrapper: function dateCellWrapperFunction() {
-          //     return <div className="bg-primary">click</div>
+          //     return <div className="bg-primary hover:bg-secondary">click</div>
           //   },
           // }}
           // components={{
@@ -174,6 +225,7 @@ const Calendar = () => {
           //   dateCellWrapper: ColoredDateCellWrapper,
           // }}
           // min={new Date()}
+          onView={onView}
         />
       </div>
       {isOpen && (
